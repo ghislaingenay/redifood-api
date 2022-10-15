05059641e1d52a9e9c32e08399bcc0346b3bcd1d
Ghislain Genay <ghislaingenay@gmail.com>
2022-10-16 00:33:51 +0700

    Gather all interfaces in one file

---
+++ src/app.interface.ts
@@ -0,0 +1,41 @@
+// Type of the food card
+export interface Food {
+  _id?: string;
+  name: string;
+  photo: string;
+  price: number;
+  description?: string;
+  section: string;
+  extra: string;
+}
+
+// Type of what the user wants (/orders)
+export interface Menu {
+  food: Food;
+  qty: number;
+}
+
+// Type for one order
+export interface Order {
+  _id: string;
+  table: number;
+  paid: boolean;
+  total: number;
+  menu: Menu[];
+  date: Date;
+}
+
+// Interface - request for history page (search: payment choice)
+export interface Request {
+  search: string;
+  datefrom: string;
+  dateto: string;
+}
+
+// Interface that concern the user (Authentification)
+export interface User {
+  name: string;
+  username: string;
+  password: string;
+  confirmpassword?: string;
+}
