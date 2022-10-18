fd8226fd23095905493b4404fca2aead0324bb97
Ghislain Genay <ghislaingenay@gmail.com>
2022-10-18 15:25:56 +0700

    Implement Get("/history") with function

--- src/history/history.controller.ts
+++ src/history/history.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Get } from '@nestjs/common';
+import { HistoryService } from './history.service';
 
 @Controller('history')
-export class HistoryController {}
+export class HistoryController {
+  constructor(private readonly historyService: HistoryService) {}
+
+  @Get()
+  getPaidOrders(
+    @Body('search') paymentChoice: string,
+    @Body('datefrom') ordersDateFrom: string,
+    @Body('dateto') ordersDateTo: string,
+  ) : void {
+    return this.historyService.getPaidOrders(paymentChoice, ordersDateFrom, ordersDateTo);
+  }
+}
--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class HistoryService {}
+export class HistoryService {
+
+  // @Get('history')
+  getPaidOrders(paymentChoice, ordersDateFrom, ordersDateTo) {
+    console.log('val', paymentChoice, ordersDateFrom, ordersDateTo);
+    // findAllOrders and filter them by paid === true
+    // And then filter them
+    // if (paymentChoice !== '') {
+    // Apply one filter
+    // }
+    // if (ordersDateFrom !== '') {
+    // Apply one filter
+    // }
+    // if (ordersDateTo !== '') {
+    // Apply one filter
+    // }
+    // Then return to users what it's left. Create a copy with spread operator and send back to user
+  }
+}
