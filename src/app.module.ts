fec0708d8009143faef7235cc360a9c67ad767ad
Ghislain Genay <ghislaingenay@gmail.com>
2022-10-18 15:10:25 +0700

    Add files for History module/controller/service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
+import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [OrdersModule, HistoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
