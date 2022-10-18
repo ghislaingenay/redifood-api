db2bfa3f27487dcc53fcd9d9ecc2a9f9c69c024f
Ghislain Genay <ghislaingenay@gmail.com>
2022-10-19 01:10:26 +0700

    Implement Delete routes + fn for "/foods"

--- src/foods/foods.controller.ts
+++ src/foods/foods.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Body, Get, Delete, Param } from '@nestjs/common';
 import { FoodsService } from './foods.service';
 import { Food, Section } from 'src/app.interface';
 
@@ -9,6 +9,29 @@ export class FoodsController {
   @Get()
   recoverFoodAndSection(): { foods: Food[]; section: Section[] } {
     return this.foodsService.recoverFoodAndSection();
+  }
+
+  @Delete('delete/section')
+  deleteSection(@Body('deleteSection') removeSection: string): {
+    foods: Food[];
+    section: Section[];
+  } {
+    return this.foodsService.deleteSection(removeSection);
+  }
+
+  // Delete one section from the FE and update DB of foods as well
+  @Delete('delete/extra')
+  deleteExtra(@Body('deleteExtra') removeExtra: string): {
+    foods: Food[];
+    section: Section[];
+  } {
+    return this.foodsService.deleteExtra(removeExtra);
+  }
+
+  //Delete a food with a specific id
+  @Delete(':id/delete')
+  deleteFood(@Param('id') foodId: string) {
+    return this.foodsService.deleteFood(foodId);
   }
 
 }
--- src/foods/foods.service.ts
+++ src/foods/foods.service.ts
@@ -4,9 +4,40 @@ import { foods as foundFoods, section as allSection } from '../data';
 @Injectable()
 export class FoodsService {
   // Recover foods and section to render in the page
+  // @Get('foods')
   recoverFoodAndSection() {
     console.log('rec');
     // Find everything in DB and return it
+    return {
+      foods: foundFoods,
+      section: allSection,
+    };
+  }
+
+  // @Delete('delete/section')
+  deleteSection(removeSection: string) {
+    // Recover the section and update foods DB and return boolean as well to confirm modifications
+    console.log('deletesection', removeSection);
+    return {
+      foods: foundFoods,
+      section: allSection,
+    };
+  }
+
+  // @Delete('delete/extra')
+  deleteExtra(removeExtra: string) {
+    // Recover the extra and update foods DB and return boolean as well to confirm modifications
+    console.log('deletesection', removeExtra);
+    return {
+      foods: foundFoods,
+      section: allSection,
+    };
+  }
+
+  // @Delete(':id/delete')
+  deleteFood(foodId: string) {
+    // Find DB of food findByIdAndDelete and update the state
+    console.log('delete food id', foodId);
     return {
       foods: foundFoods,
       section: allSection,
