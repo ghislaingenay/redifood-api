e188c66210f8fa65e395ef55d3737b156de4c3dc
Ghislain Genay <ghislaingenay@gmail.com>
2022-10-20 00:45:40 +0700

    Create foodSchema for DB (MongoDB)

---
+++ src/schemas/foods.schema.ts
@@ -0,0 +1,26 @@
+import * as mongoose from 'mongoose';
+
+export const FoodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: String,
+  // Could implement a DB for section and section back the id
+  section: {
+    type: String,
+    required: true,
+  },
+  extra: {
+    type: String,
+    required: true,
+  }
+});
