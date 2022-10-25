e80acf990867d409d2b2fd4731e783dd45c8882d
Ghislain Genay <ghislaingenay@gmail.com>
2022-10-25 19:39:34 +0700

    Implement basics Get/post roiutes for auth/login and auth/signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  getAuthentification(): {isAuthenticated : boolean} {
+    this.authService.getAuthentification()
+  };
+
+  @Post('signup')
+  signup(
+    @Body('name') nameUser: string,
+    @Body('username') userName: string,
+    @Body('password') passwordUser: string,
+  ): boolean {
+    const dto = {
+      name: nameUser,
+      username: userName,
+      password: passwordUser,
+    };
+    return this.authService.signup(dto);
+  }
+
+  @Post('login')
+  login(
+    @Body('name') nameUser: string,
+    @Body('username') userName: string,
+    @Body('password') passwordUser: string,
+  ): boolean {
+    const dto = {
+      name: nameUser,
+      username: userName,
+      password: passwordUser,
+    };
+    return this.authService.login(dto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/app.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  // @Get('/auth')
+  getAuthentification() {
+    return { isAuthenticated: false };
+  }
+
+  // @Post('auth/login')
+  login(dto: User) {
+    console.log(dto);
+    // find the user by email
+    // if user does not exist, throw excetpt
+    return false;
+  }
+
+  // @Post('auth/signup')
+  signup(dto: User) {
+    console.log(dto);
+    // find the user by email
+    // if user does not exist throw exception
+    // Compare password
+    // if password incorrect => throw exception
+    // Otherwise, send data
+    // if (!user) throw new ForbiddenExeption ('Credentials incorrect')
+    // if (wrons password) throw new ForbiddenExeption ('Credentials incorrect')
+    // delete user.hash
+    return false;
+  }
+}
