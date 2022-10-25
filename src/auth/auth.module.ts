1fd9bae585de6827609feca91953497cfca11a8f
Ghislain Genay <ghislaingenay@gmail.com>
2022-10-25 21:11:55 +0700

    Fix schema error MongooseModule in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { USER_MODEL } from 'constant';
+import { UserSchema } from 'src/schemas/users.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: USER_MODEL, schema: USER_MODEL }]),
+    MongooseModule.forFeature([{ name: USER_MODEL, schema: UserSchema }]),
   ],
   controllers: [AuthController],
   providers: [AuthService],
