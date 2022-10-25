6d271dc993c5fb8a6ac0e460d0c14bf4249fe7a4
Ghislain Genay <ghislaingenay@gmail.com>
2022-10-26 00:31:05 +0700

    Add basics code for express-session & cookies stored in DB

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
+import * as passport from 'passport';
+import { default as connectMongoDBSession } from 'connect-mongodb-session';
+
+const MongoDBStore = connectMongoDBSession(session);
+
+const sessionStore = new MongoDBStore({
+  uri: process.env.MONGODB_URL,
+  collection: 'sessions',
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +20,24 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  app.use(
+    session({
+      secret: `${process.env.SESSION_SECRET}`,
+      resave: false,
+      saveUninitialized: false,
+      rolling: true,
+      unset: 'destroy',
+      store: sessionStore,
+      cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week in milliseconds
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+
   await app.listen(3003);
 }
 bootstrap();
