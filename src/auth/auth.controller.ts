16cca34086ed2d0ea3a3fbd7d5c632d533e7bc07
Ghislain Genay <ghislaingenay@gmail.com>
2022-10-27 02:12:09 +0700

    Fix issues related to PassportModule.register + comment all auth.controller
    PassportModule.register was previously located

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,36 +5,36 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('login')
-  getAuthentification(): void {
-    this.authService.getAuthentification();
-  }
+  // @Get('login')
+  // getAuthentification(): void {
+  //   this.authService.getAuthentification();
+  // }
 
-  @Post('signup')
-  async signup(
-    @Body('name') nameUser: string,
-    @Body('username') userName: string,
-    @Body('password') passwordUser: string,
-  ): Promise<any> {
-    const dto = {
-      name: nameUser,
-      username: userName,
-      password: passwordUser,
-    };
-    return await this.authService.signup(dto);
-  }
+  // @Post('signup')
+  // async signup(
+  //   @Body('name') nameUser: string,
+  //   @Body('username') userName: string,
+  //   @Body('password') passwordUser: string,
+  // ): Promise<any> {
+  //   const dto = {
+  //     name: nameUser,
+  //     username: userName,
+  //     password: passwordUser,
+  //   };
+  //   return await this.authService.signup(dto);
+  // }
 
-  @Post('login')
-  async login(
-    @Body('name') nameUser: string,
-    @Body('username') userName: string,
-    @Body('password') passwordUser: string,
-  ): Promise<any> {
-    const dto = {
-      name: nameUser,
-      username: userName,
-      password: passwordUser,
-    };
-    return await this.authService.login(dto);
+  // @Post('login')
+  // async login(
+  //   @Body('name') nameUser: string,
+  //   @Body('username') userName: string,
+  //   @Body('password') passwordUser: string,
+  // ): Promise<any> {
+  //   const dto = {
+  //     name: nameUser,
+  //     username: userName,
+  //     password: passwordUser,
+  //   };
+  //   return await this.authService.login(dto);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { USER_MODEL } from 'constant';
-import { UserSchema } from 'src/schemas/users.schema';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
 import { SessionSerializer } from './session.serializer';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: USER_MODEL, schema: UserSchema }]),
-    PassportModule,
-  ],
-  controllers: [AuthController, PassportModule.register({ session: true })],
+  imports: [PassportModule.register({ session: true })],
+  // controllers: [AuthController],
   providers: [AuthService, LocalStrategy, SessionSerializer],
 })
 export class AuthModule {}
