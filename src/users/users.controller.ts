7d5920691c303bdb1107d4b93d14ada9e5d202ad
Ghislain Genay <ghislaingenay@gmail.com>
2022-11-05 22:33:11 +0700

    Reimplement previously deleted routes in users + local-auth.guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,12 @@ import {
   Controller,
   Get,
   Post,
-  Body,
   UseGuards,
-  Request,
-  Response,
+  Request as Req,
+  Response as Res,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import * as bcrypt from 'bcrypt';
-import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
+import { Request } from 'express';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 import { UsersService } from './users.service';
 
@@ -17,9 +15,29 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(AuthGuard('jwt'))
-  @Get('me')
-  getMe() {
-    return 'user info';
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Req() req): Promise<any> {
+    console.log(req.body);
+    return { User: req.user, msg: 'User logged in' };
   }
+
+  //Get / protected
+  @Get('protected')
+  async getHello(@Req() req, @Res() res): Promise<any> {
+    console.log(req.user);
+    res.json(req.user);
+  }
+
+  //Get / logout
+  @Get('/logout')
+  async logout(@Req() req, @Res() res): Promise<any> {
+    res.json({ msg: 'The user session has ended' });
+  }
+
+  // @UseGuards(AuthGuard('jwt'))
+  // @Get('me')
+  // getMe(@Req() req: Request) {
+  //   return req.user;
+  // }
 }
