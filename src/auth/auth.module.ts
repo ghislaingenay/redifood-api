4f6144323ea9c06e64ba16f587fb7e0c62226500
Ghislain Genay <ghislaingenay@gmail.com>
2022-11-05 16:50:48 +0700

    Remove info for passport and express-session

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PassportModule } from '@nestjs/passport';
-import { LocalStrategy } from './local.strategy';
-import { SessionSerializer } from './session.serializer';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [UsersModule, JwtModule.register({})],
-  // controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, SessionSerializer, JwtStrategy],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as session from 'express-session';
-import * as passport from 'passport';
-import * as connectMongoDBSession from 'connect-mongodb-session';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,35 +14,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  const MongoDBStore = connectMongoDBSession(session);
-
-  const sessionStore = new MongoDBStore({
-    uri: process.env.MONGODB_URL,
-    collection: 'sessions',
-  });
-  app.use(
-    session({
-      secret: `${process.env.SESSION_SECRET}`,
-      resave: false,
-      saveUninitialized: false,
-      rolling: true,
-      unset: 'destroy',
-      store: sessionStore,
-      cookie: {
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week in milliseconds
-      },
-    }),
-  );
-
-  app.use(cookieParser());
-
-  app.use(
-    passport.initialize({
-      userProperty: 'user',
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
 
   await app.listen(3003);
 }
