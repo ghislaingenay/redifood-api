d22223b88b1aa3c169c664a2c609940e9700ac3a
Ghislain Genay <ghislaingenay@gmail.com>
2022-11-24 21:28:18 +0700

    Remove unusued routes (get /auth and logout)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,9 @@
-import { Controller, Post, Get, Response, Request } from '@nestjs/common';
+import { Controller, Post, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @Get('login')
-  accessLogin(@Request() req, @Response() res) {}
-
-  @Get('signup')
-  accessSignUp(@Request() req, @Response() res) {}
-
-  @Get('logout')
-  logoutAccess(@Request() req, @Response() res) {
-    req.logout();
-    req.session.destroy((err) => {
-      if (err) {
-        throw new Error('Impossible to remove the current session');
-      }
-      res.clearCookie('connect.sid');
-      res.json({ isLoggedOut: true });
-    });
-  }
 
   @Post('signup')
   async signup(@Request() req) {
